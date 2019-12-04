69c783042ec962eea42c4a9e2c10ca78064e0d23
piotrek <piotrek@domek.com>
2019-12-04 21:51:53 +0100

    Day3/exercise1 - improved with sets

--- day3/ex1.py
+++ day3/ex1.py
@@ -1,63 +1,36 @@
-board = dict()
-sumlen = 0
+def draw(board, commands):
+    c_x, c_y = 0, 0
+    for command in commands:
+        direction = command[0]
+        length = int(command[1:])
+        if direction == "L":
+            for x in range(c_x - length, c_x):
+                board.add((x, c_y))
+            c_x = c_x - length
+        if direction == "R":
+            for x in range(c_x + 1, c_x + length + 1):
+                board.add((x, c_y))
+            c_x = c_x + length
+        if direction == "D":
+            for y in range(c_y - length, c_y):
+                board.add((c_x, y))
+            c_y = c_y - length
+        if direction == "U":
+            for y in range(c_y + 1, c_y + length + 1):
+                board.add((c_x, y))
+            c_y = c_y + length
+
+
 with open('input.txt') as f:
     commands = f.readline().strip().split(',')
-    c_x, c_y = 0, 0
-    for command in commands:
-        direction = command[0]
-        length = int(command[1:])
-        if direction == "L":
-            for x in range(c_x - length, c_x):
-                board[(x, c_y)] = 1
-            c_x = c_x - length
-        if direction == "R":
-            for x in range(c_x + 1, c_x + length + 1):
-                board[(x, c_y)] = 1
-            c_x = c_x + length
-        if direction == "D":
-            for y in range(c_y - length, c_y):
-                board[(c_x, y)] = 1
-            c_y = c_y - length
-        if direction == "U":
-            for y in range(c_y + 1, c_y + length + 1):
-                board[(c_x, y)] = 1
-            c_y = c_y + length
-
-
+    board1 = set()
+    draw(board1, commands)
 
     commands = f.readline().strip().split(',')
-    c_x, c_y = 0, 0
-    for command in commands:
-        direction = command[0]
-        length = int(command[1:])
-        if direction == "L":
-            for x in range(c_x - length, c_x):
-                if board.get((x, c_y), 2) != 2:
-                    board[(x, c_y)] = 'X'
-                else:
-                    board[(x, c_y)] = 2
-            c_x = c_x - length
-        if direction == "R":
-            for x in range(c_x + 1, c_x + length + 1):
-                if board.get((x, c_y), 2) != 2:
-                    board[(x, c_y)] = 'X'
-                else:
-                    board[(x, c_y)] = 2
-            c_x = c_x + length
-        if direction == "D":
-            for y in range(c_y - length, c_y):
-                if board.get((c_x, y), 2) != 2:
-                    board[(c_x, y)] = 'X'
-                else:
-                    board[(c_x, y)] = 2
-            c_y = c_y - length
-        if direction == "U":
-            for y in range(c_y + 1, c_y + length + 1):
-                if board.get((c_x, y),2) != 2:
-                    board[(c_x, y)] = 'X'
-                else:
-                    board[(c_x, y)] = 2
-            c_y = c_y + length
+    board2 = set()
+    draw(board2, commands)
 
-    crossings = [abs(k[0]) + abs(k[1]) for k in board if board[k] == "X"]
-    print(min(crossings))
+    crossings = board1 & board2
+
+    min_distance =min((abs(k[0]) + abs(k[1]) for k in crossings))
+    print(min_distance)
