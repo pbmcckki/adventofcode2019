97e85222a68768d681fe576af6b1dd69d740120d
piotrek <piotrek@domek.com>
2019-12-09 23:16:24 +0100

    Day8/ex2 with reduce

--- day8/ex2.py
+++ day8/ex2.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 layers = []
 layer_size = 25 * 6
 count = 0
@@ -11,13 +13,19 @@ with open('input.txt') as f:
 image = []
 layers_stacked = list(zip(*layers))
 
+
+def flatten(i, j):
+    if i != '2':
+        return i
+    else:
+        return j
+
+
 for pixel in layers_stacked:
-    for color in pixel:
-        if color != '2':
-            image.append(color)
-            break
+    image.append(reduce(flatten, pixel))
+
 # Do some repainting for clearer view :)
-image=[' ' if x =='0' else "X" for x in image]
+image = [' ' if x == '0' else "X" for x in image]
 
 for y in range(0, 6):
     for x in range(0, 25):
