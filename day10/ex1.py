4c0ceb0ef0c3d423c25c239e47083e669cc07856
piotrek <piotrek@domek.com>
2019-12-11 21:00:53 +0100

    Day10/ex1 - optimized :-) Added some test files and main input

--- day10/ex1.py
+++ day10/ex1.py
@@ -1,3 +1,4 @@
+# This is tricky description to find larges common denominator
 def find_denominator(a, b):
     a, b = abs(a), abs(b)
     if a == 0 or b == 0:
@@ -13,7 +14,7 @@ def find_denominator(a, b):
 bitmap = []
 locations = []
 result = dict()
-with open('test2.txt') as f:
+with open('input.txt') as f:
     for line in f:
         bitmap.append(line.strip())
 
@@ -25,46 +26,21 @@ locations = [(x, y) for y in range(0, y_size) for x in range(0, x_size) if bitma
 for location in locations:
     # Calculate vector
     vectors = [(x[0] - location[0], x[1] - location[1]) for x in locations if (x[0], x[1]) != location]
-    # print(location, vectors)
-    # Reduce vectors
-    vectors_reduced = vectors
-    for element in vectors:
 
+    # Reduce vectors to angles - each angle is separate asteroids, but each does not count twice so we use set
+    angles = set()
+
+    # Reduce each element by largest common denominator (N)
+    # Each angle is N*X,N*Y so can be reduced by N and still be the same angle!
+
+    for element in vectors:
         denominator = find_denominator(*element)
         root_element = (element[0] / denominator, element[1] / denominator)
-        # print(" ELEMENT: {} ROOT: {}".format(element,root_element))
-        for element_tested in vectors:
-            if element_tested == element:
-                continue
 
-            # Root is horizontal and element is horizontal
-            if (root_element[0] != 0) and (element_tested[1] == root_element[1] == 0):
-                # Root and tested element have same X direction
-                if root_element[0] * element_tested[0] > 0:
-                    vectors_reduced.remove(element_tested)
-                    # print("REMOVING {}   ".format(element_tested),end="")
+        angle = root_element
+        angles.add(angle)
 
-            # Root is vertical and element is vertical
-            elif (root_element[0] == element_tested[0] == 0) and (root_element[1] != 0):
-                # Root and tested element have same Y direction
-                if root_element[1] * element_tested[1] > 0:
-                    vectors_reduced.remove(element_tested)
-                    # print("REMOVING {}   ".format(element_tested),end="")
+    result[len(angles)] = location
 
-            # Root is not perpendicular ;-)
-            elif (element_tested[0] != 0) and (element_tested[1] != 0):
-                # Root and tested element have same directions
-                if (root_element[0] * element_tested[0] > 0) and (root_element[1] * element_tested[1] > 0):
-                    # Element is multiplication of root (same factor for x and y)
-                    if (element_tested[0] % root_element[0] == 0) and (element_tested[1] % root_element[1] == 0) and (
-                            element_tested[0] / root_element[0] == element_tested[1] / root_element[1]):
-                        # print("REMOVING {}   ".format(element_tested),end="")
-                        vectors_reduced.remove(element_tested)
-            # print(vectors_reduced)
-    # print('-'*20)
-
-    view = len(vectors_reduced)
-    print(location, view)
-    result[view] = location
 largest_view = max(result.keys())
 print(largest_view, result[largest_view])
